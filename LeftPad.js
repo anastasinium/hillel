0c6c53162756e788b840f59b854d3320d272634f
Anastasia <nastia21.11@gmail.com>
2017-06-05 14:25:33 +0300

    left pad

--- LeftPad.js
+++ LeftPad.js
@@ -1,3 +1,32 @@
-function () {
-}
+window.onload = function loadPage() {
 
+//
+//function leftPad() {
+//var text = document.getElementById('text').value;
+//
+//if (text.length === 5) {
+//    return text;
+//}
+
+
+//}
+
+var add = document.getElementById('add');
+
+add.addEventListener('click', function() {
+    var text = document.getElementById('text').value;
+    var save = document.getElementById('save');
+    console.log(text.length);
+
+
+    if (text.length === 5) {
+        return text;
+    } else if (text.length < 5) {
+
+    }
+
+     save.innerHTML = text;
+
+});
+
+}
